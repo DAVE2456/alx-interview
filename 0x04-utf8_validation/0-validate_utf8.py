d54747fb491c1b92ec21cd0a6f80f5baaf4fe0b5
DAVE2456 <davehunegnaw@gmail.com>
2023-04-19 16:04:04 +0300

    initial commit

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3                                                              
+                                                                                
+"""UTF-8 Validation"""                                                                                                                                          
+                                                                                
+def validUTF8(data):                                                            
+    """                                                                         
+       Determines if given data set                                             
+       represents a valid UTF-8 encoding                                            """                                                                         
+    def check(num):                                                             
+        mask = 0b1000000 # 128                                                          i = 0                                                                   
+        while num & mask: # 11000110(198) & 10000000(128)                       
+            mask >>= 1                                                          
+            i += 1                                                                      return i                                                                
+                                                                                
+    i = 0                                                                       
+    while i < len(data):                                                        
+        j = check(data[i])                                                      
+        a = i + j - (j != 0)                                                    
+        i += 1                                                                  
+        if j == 1 orj > 4 or a >= len(data)                                     
+            return False                                                        
+        while i < len (data) and i <= a:                                        
+            curnt = check(data[i])                                              
+            if curnt != 1:                                                      
+                return False                                                    
+            i += 1                                                              
+    return True
