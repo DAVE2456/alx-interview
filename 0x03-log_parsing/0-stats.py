b673a886534137a980e6df74c3cb5eca3c02b816
DAVE2456 <davehunegnaw@gmail.com>
2023-04-20 18:03:16 +0300

    correction code

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,40 +1,36 @@
 #!/usr/bin/python3
 
-import re
+
 import sys
 status_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
                 '403': 0, '404': 0, '405': 0, '500': 0}
- print_counter= 0
+print_counter= 0
 size_summation = 0
 
+try:
+     for line in sys.stdin:
+         line_list = line.split(" ")
+         if len(line_list) > 4:
+             code = line_list[-2]
+             size = int(line_list[-1])
+             if code in status_codes.keys():
+                 status_codes[code] += 1
+             size_summation += size
+             print_counter += 1
 
-def print_logs():
-    """
-    Prints status codes to the logs
-    """
-    print(f"File size: {size_summation}")
-    for k, v in sorted(status_codes.items()):
-        if v != 0:
-            print(f"{k}: {v}")
+         if print_counter == 10:
+             print_counter = 0
+             print('File size: {}'.format(size_summation))
+             for key, value != 0:
+                 if value != 0:
+                     print('{}: {}'.format(key, value))
 
+except Exception as err:
+    pass
 
-if __name__ == "__main__":
-    try:
-        for line in sys.stdin:
-            std_line = line.replace("\n", "")
-            log_list = re.split('- | "|" | " " ', str(std_line))
-            try:
-                codes = log_list[-1].split(" ")
-                if int(codes[0]) not in status_codes.keys():
-                    continue
-                status_codes[int(codes[0])] += 1
-                print_counter += 1
-                size_summation += int(codes[1])
-                if print_counter % 10 == 0:
-                    print_logs()
-            except:
-                pass
-        print_logs()
-    except KeyboardInterrupt:
-        print_logs()
-
+    finally:
+        print('File size: {}'.format(size_summation))
+        for key, value in sorted(status_codes.items()):
+            if value != 0:
+                print('{}: {}'.format(key, value))
+  
