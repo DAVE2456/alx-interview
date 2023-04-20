6f9467244a17d42e87fb892ecc5de9965c69d3ae
DAVE2456 <davehunegnaw@gmail.com>
2023-04-20 17:03:20 +0300

    correction

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 
-import re
 import sys
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
-                403: 0, 404: 0, 405: 0, 500: 0}
+status_codes = {'200: 0, 301: 0, 400: 0, 401: 0,
+                '403: 0, 404: 0, 405: 0, 500: 0}
 print_counter = 0
 size_summation = 0
 
