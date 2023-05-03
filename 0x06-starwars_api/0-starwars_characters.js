eb703562add5fe50e0fa3e0e8b7f4af6a2971da6
DAVE2456 <davehunegnaw@gmail.com>
2023-05-03 12:26:22 +0300

    correction code

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,7 +3,7 @@ const request = require('request')
 
 
 const movieId = process.argv[2];
-const endpoint = `https://swapi-api.hbtn.io/api/${movieId}`;
+const endpoint = `https://swapi-api.hbtn.io/api/films${movieId}`;
 
 request(url, async (err, res, _body) => {
   if (err) {
