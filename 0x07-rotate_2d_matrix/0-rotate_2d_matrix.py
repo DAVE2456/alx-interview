7b25a4da57b4f4c1c0f29effea463cab5e320b86
DAVE2456 <davehunegnaw@gmail.com>
2023-05-11 11:07:24 +0300

    2d matrix correction code

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,11 +1,14 @@
-#!/usr/bin/python3
+#!usr/bin/python3
+
+"""rotate 2d matrix"""
+
 
 def rotate_2d_matrix(matrix):
     n = len(matrix)
-    # Step 1: Transpose the matrix
+    # Transpose the matrix
     for i in range(n):
         for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-    # Step 2: Reverse each row of the transposed matrix
+    # Reverse each row
     for i in range(n):
         matrix[i] = matrix[i][::-1]
