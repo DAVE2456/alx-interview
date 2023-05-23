00dceed86b63326a8dd3ecd1c59a69d336b82a89
DAVE2456 <davehunegnaw@gmail.com>
2023-05-23 13:15:45 +0300

    correction code

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -2,16 +2,16 @@
 
 
 def island_perimeter(grid):
+
     perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
                 perimeter += 4
-                # cells with 2 sides touching other cells on top and bottom
-                if i > 0 and grid[i - 1][j] == 1:
+                if i > 0 and grid[i-1][j] == 1:
                     perimeter -= 2
-                # cells with 2 sides touching other cells left and right
-                if j > 0 and grid[i][j - 1] == 1:
+                if j > 0 and grid[i][j-1] == 1:
                     perimeter -= 2
 
     return perimeter
+
